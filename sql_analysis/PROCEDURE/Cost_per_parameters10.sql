046095892bae37f1a844b572d9aca42acbc62c0e
Nick <kalligerisnick@gmail.com>
2026-01-18 20:57:31 +0200

    Add all SQL scripts, Procedure and VIEW folders

---
+++ sql_analysis/PROCEDURE/Cost_per_parameters10.sql
@@ -0,0 +1,56 @@
+DELIMITER $$
+
+CREATE PROCEDURE beneficiarysummarysample_2010(IN v_gender_disease VARCHAR(50))
+BEGIN
+    SET @sql = CONCAT(
+        'SELECT 
+            CASE WHEN gender_code = 1 THEN "Male" ELSE "Female" END AS Gender,
+            COUNT(*) AS Patient_Count,
+            ROUND(AVG(payment_inpatient), 2) AS Avg_Inpatient_Cost,
+            ROUND(AVG(payment_outpatient), 2) AS Avg_Outpatient_Cost
+         FROM beneficiarysummarysample_2010
+         WHERE ', v_gender_disease, ' = 1
+         GROUP BY gender_code'
+    );
+    PREPARE stmt FROM @sql; EXECUTE stmt; DEALLOCATE PREPARE stmt;
+END$$
+
+CREATE PROCEDURE beneficiarysummarysample_2010(IN v_age_disease VARCHAR(50))
+BEGIN
+    SET @sql = CONCAT(
+        'SELECT 
+            CASE 
+                WHEN (2010 - LEFT(birth_date, 4)) < 45 THEN "Under 45"
+                WHEN (2010 - LEFT(birth_date, 4)) BETWEEN 45 AND 64 THEN "45-64"
+                WHEN (2010 - LEFT(birth_date, 4)) BETWEEN 65 AND 74 THEN "65-74"
+                WHEN (2010 - LEFT(birth_date, 4)) BETWEEN 75 AND 84 THEN "75-84"
+                ELSE "85+" 
+            END AS Age_Group,
+            COUNT(*) AS Patient_Count,
+            ROUND(AVG(payment_inpatient), 2) AS Avg_Inpatient_Cost,
+            ROUND(AVG(payment_outpatient), 2) AS Avg_Outpatient_Cost
+         FROM beneficiarysummarysample_2010
+         WHERE ', v_age_disease, ' = 1
+         GROUP BY Age_Group
+         ORDER BY Age_Group'
+    );
+    PREPARE stmt FROM @sql; EXECUTE stmt; DEALLOCATE PREPARE stmt;
+END$$
+
+CREATE PROCEDURE beneficiarysummarysample_2010(IN v_state_disease VARCHAR(50))
+BEGIN
+    SET @sql = CONCAT(
+        'SELECT 
+            state_code,
+            COUNT(*) AS Patient_Count,
+            ROUND(AVG(payment_inpatient), 2) AS Avg_Inpatient_Cost,
+            ROUND(SUM(payment_inpatient), 2) AS Total_State_Spending
+         FROM beneficiarysummarysample_2010
+         WHERE ', v_state_disease, ' = 1
+         GROUP BY state_code
+         ORDER BY Avg_Inpatient_Cost DESC'
+    );
+    PREPARE stmt FROM @sql; EXECUTE stmt; DEALLOCATE PREPARE stmt;
+END$$
+
+DELIMITER ;
---
+++ sql_analysis/VIEW/Costly_diseases_view10.sql
@@ -0,0 +1,70 @@
+-- ==========================================
+-- PACKAGE: MEDICARE_DATA_MINING_TOOLS
+-- Description: Creation of view for finding the costliest diseases
+-- Author: [Nick Kalligeris]
+-- ==========================================
+
+CREATE OR REPLACE VIEW Costly_diseases_view AS
+	SELECT 'alzheimers_disease' AS Disease, ROUND(AVG(payment_inpatient + payment_outpatient), 2) AS Avg_Total_Cost 
+	FROM beneficiarysummarysample_2010 
+	WHERE alzheimers_disease = 1
+
+	UNION ALL
+
+	SELECT 'congestive_heart_failure', ROUND(AVG(payment_inpatient + payment_outpatient), 2) 
+	FROM beneficiarysummarysample_2010 
+	WHERE congestive_heart_failure = 1
+
+	UNION ALL
+
+	SELECT 'chronic_kidney_disease', ROUND(AVG(payment_inpatient + payment_outpatient), 2) 
+	FROM beneficiarysummarysample_2010 
+	WHERE chronic_kidney_disease = 1
+
+	UNION ALL
+
+	SELECT 'cancer', ROUND(AVG(payment_inpatient + payment_outpatient), 2) 
+	FROM beneficiarysummarysample_2010 
+	WHERE cancer = 1
+
+	UNION ALL
+
+	SELECT 'copd', ROUND(AVG(payment_inpatient + payment_outpatient), 2) 
+	FROM beneficiarysummarysample_2010 
+	WHERE copd = 1
+
+	UNION ALL
+
+	SELECT 'depression', ROUND(AVG(payment_inpatient + payment_outpatient), 2) 
+	FROM beneficiarysummarysample_2010 
+	WHERE depression = 1
+
+	UNION ALL
+
+	SELECT 'diabetes', ROUND(AVG(payment_inpatient + payment_outpatient), 2) 
+	FROM beneficiarysummarysample_2010 
+	WHERE diabetes = 1
+
+	UNION ALL
+
+	SELECT 'ischemic_heart_disease', ROUND(AVG(payment_inpatient + payment_outpatient), 2) 
+	FROM beneficiarysummarysample_2010 
+	WHERE ischemic_heart_disease = 1
+
+	UNION ALL
+
+	SELECT 'osteoporosis', ROUND(AVG(payment_inpatient + payment_outpatient), 2) 
+	FROM beneficiarysummarysample_2010 
+	WHERE osteoporosis = 1
+
+	UNION ALL
+
+	SELECT 'rheumatoid_arthritis_oa', ROUND(AVG(payment_inpatient + payment_outpatient), 2) 
+	FROM beneficiarysummarysample_2010 
+	WHERE rheumatoid_arthritis_oa = 1
+
+	UNION ALL
+
+	SELECT 'stroke', ROUND(AVG(payment_inpatient + payment_outpatient), 2) 
+	FROM beneficiarysummarysample_2010 
+	WHERE stroke = 1
